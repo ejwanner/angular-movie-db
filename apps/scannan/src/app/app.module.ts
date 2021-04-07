dd3aa3e3c43a12886010d8f8056a73a98eb889c1
Elias Wanner <elias.wanner.15@gmail.com>
2021-04-07 21:38:37 +0200

    add ng-zorro to the app

--- apps/scannan/src/app/app.module.ts
+++ apps/scannan/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    NzSwitchModule,
+    NzButtonModule,
+    NzTableModule,
+    NzLayoutModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
